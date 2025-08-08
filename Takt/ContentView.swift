65d03aa456ca1970ac70ae6c0aabbe4786646ebc
mi11ione <i@mi11ion.io>
2025-08-08 03:30:18 +0300

    Initial Commit

---
+++ Takt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Takt
+//
+//  Created by mi11ion on 8/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Takt/TaktApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaktApp.swift
+//  Takt
+//
+//  Created by mi11ion on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TaktApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
